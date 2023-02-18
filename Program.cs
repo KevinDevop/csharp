06952e2dbd0dff07be7229fdbc5e33b3cf10b641
KevinDevop <110274931+KevinDevop@users.noreply.github.com>
2023-02-17 22:21:29 -0500

    Tipos de variables.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,32 @@
 ﻿
+//TIPOS DE DATOS
 
-var nombre = "Kevin";
+using System.Data.SqlTypes;
 
-Console.WriteLine(nombre);
+byte bytes = 255;
+Console.WriteLine(bytes);
+
+int numero = 333;
+Console.WriteLine(numero);
+
+bool afirmacion = true;
+Console.WriteLine(afirmacion);
+
+string texto = "Hola inmundo animal!";
+Console.WriteLine(texto);
+
+float flotantes = 121.0f;
+Console.WriteLine(flotantes);
+
+decimal decimales = 1201.5m;
+Console.WriteLine(decimales);
+
+//El signo hace la variable recibir null
+int? nullable = null;
+Console.WriteLine(nullable);
+
+var texto2 = "Hola";
+Console.WriteLine(texto2);
+
+var kevin = new { nombre = "Kevin" , edad = 23, ciudad = "Barranquilla"};
+Console.WriteLine(kevin.edad);
