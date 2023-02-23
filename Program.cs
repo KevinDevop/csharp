92781998b96af800ae2ed1286367247158c71a02
KevinDevop <110274931+KevinDevop@users.noreply.github.com>
2023-02-22 20:41:43 -0500

    Funciones

--- Program.cs
+++ Program.cs
@@ -1,32 +1,66 @@
 ﻿
-//TIPOS DE DATOS
 
-using System.Data.SqlTypes;
+namespace csharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //TIPOS DE DATOS
 
-byte bytes = 255;
-Console.WriteLine(bytes);
+            byte bytes = 255;
+            Console.WriteLine(bytes);
 
-int numero = 333;
-Console.WriteLine(numero);
+            int numero = 333;
+            Console.WriteLine(numero);
 
-bool afirmacion = true;
-Console.WriteLine(afirmacion);
+            bool afirmacion = true;
+            Console.WriteLine(afirmacion);
 
-string texto = "Hola inmundo animal!";
-Console.WriteLine(texto);
+            string texto = "Hola inmundo animal!";
+            Console.WriteLine(texto);
 
-float flotantes = 121.0f;
-Console.WriteLine(flotantes);
+            float flotantes = 121.0f;
+            Console.WriteLine(flotantes);
 
-decimal decimales = 1201.5m;
-Console.WriteLine(decimales);
+            decimal decimales = 1201.5m;
+            Console.WriteLine(decimales);
 
-//El signo hace la variable recibir null
-int? nullable = null;
-Console.WriteLine(nullable);
+            //El signo hace la variable recibir null
+            int? nullable = null;
+            Console.WriteLine(nullable);
 
-var texto2 = "Hola";
-Console.WriteLine(texto2);
+            var texto2 = "Hola";
+            Console.WriteLine(texto2);
 
-var kevin = new { nombre = "Kevin" , edad = 23, ciudad = "Barranquilla"};
-Console.WriteLine(kevin.edad);
+            var kevin = new { nombre = "Kevin", edad = 23, ciudad = "Barranquilla" };
+            Console.WriteLine(kevin.edad);
+
+            static string Mensaje(string nombre)
+            {
+                return $"Hola {nombre}";
+            }
+
+
+        }
+        public static int sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
